dc03a2979dba8425eba8ef90bea6eee725ebcafe
Soudagh <fr.kristina@inbox.ru>
2023-07-27 05:12:49 +0700

    some gradle and database fixes

--- app/src/main/java/com/example/repeatit/data/RoomDatabase.kt
+++ app/src/main/java/com/example/repeatit/data/RoomDatabase.kt
@@ -7,9 +7,11 @@ import androidx.room.RoomDatabase
 import com.example.repeatit.data.dao.ItemDao
 import com.example.repeatit.data.dao.ThemeDao
 import com.example.repeatit.data.dao.UserDao
+import com.example.repeatit.data.entities.Item
 import com.example.repeatit.data.entities.Theme
+import com.example.repeatit.data.entities.User
 
-@Database(entities = [Theme::class], version = 1, exportSchema = false)
+@Database(entities = [Theme::class, Item::class, User::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun themeDao() : ThemeDao
